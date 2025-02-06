c99f72ea25ef2f25b8b9740e06f9b383541b2425
kasidin <kasidinkhamfoo@gmail.com>
2025-02-06 11:47:58 +0700

    main_game updated

--- main_game.cpp
+++ main_game.cpp
@@ -7,11 +7,39 @@ void combat();
 void combatHUD();
 void Moving();
 void monfight();
+void npc();
+void market();
+void startHud();
+
 
 int level = 0, xp =0,Hp =0, totalHp=0, xptolevel=0, maxhp=0;
 int monHp =0, monXp =0,monlvl=0;
 
 
 int main(){
+    
+}
+
+void HUD(){
+
+}
+
+void combat(){
+
+}
+
+void combatHUD(){
+
+}
+
+void Moving(){
+
+}
+
+void monfight(){
+
+}
+
+void npc(){
     
 }
