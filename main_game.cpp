4fdbf64898dc534e8bdd67d3b319e5b7aa05dfe8
kasidin <kasidinkhamfoo@gmail.com>
2025-02-06 11:40:44 +0700

    main_game kub

---
+++ main_game.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <windows.h>
+#include <string>
+
+void HUD();
+void combat();
+void combatHUD();
+void Moving();
+void monfight();
+
+int level = 0, xp =0,Hp =0, totalHp=0, xptolevel=0, maxhp=0;
+int monHp =0, monXp =0,monlvl=0;
+
+
+int main(){
+    
+}
