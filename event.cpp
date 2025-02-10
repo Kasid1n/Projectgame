7efac434d7f110838d5be9663522909e145fd06a
Chiraphat-t <chiraphat.ttt@gmail.com>
2025-02-10 21:45:30 +0700

    event01

---
+++ event.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void mon();
+void shop();
+void box();
+void npc();
+void treasure();
+void boss();
+
+void event(int &i){
+   int r = rand()%100+1;
+
+  if(i<10) {
+    if(r<=70){
+      mon;
+    }
+    else if(r>70 && r<=85){
+      shop;
+    }
+    else if(r>85 && r<=90){
+      box;
+    }
+    else if(r>90 && r<=100){
+      npc;
+    }
+    i++;
+  }
+
+  else if(i>=10){
+        if(r<=65){
+          mon;
+        }
+        else if(r>65 && r<=75){
+          shop;
+        }
+        else if(r>75 && r<=80){
+          box;
+        }
+        else if(r>80 && r<=90){
+          npc;
+        }
+        else if(r>90 && r<=95){
+          treasure;
+        }
+        else if(r>95 && r<=100){
+          boss;
+        }
+        i++;
+  }
+
+}
+
+
+
+void box(){
+  int r = rand()%2;
+  if (r==0)
+  {
+    //get item
+  }
+
+  if (r==1)
+  {
+    // -hp
+  }
+
+}
