9392f666ab45ef9dcba91ca6b3f31bc3dd85c499
Chiraphat-t <chiraphat.ttt@gmail.com>
2025-02-17 22:40:12 +0700

    Equipment.h

--- Equipment.h
+++ Equipment.h
@@ -10,8 +10,8 @@ class Equipment{
 	int hpmax;
 	int atk;
 	int def;
-    int magic;
-    int speed;
+   	int magic;
+   	int speed;
 	public:
 		Equipment(int,int,int,int,int);
 		vector<int> getStat();			
@@ -21,8 +21,8 @@ Equipment::Equipment(int h,int a,int d,int m, int s){
 	hpmax = h;
 	atk = a;
 	def = d;
-    magic = m;
-    speed = s;
+    	magic = m;
+    	speed = s;
 
 }
 
@@ -31,27 +31,41 @@ vector<int> Equipment::getStat(){
     return stats;
 }
 
-/*void Unit::equip(Equipment *a){
-	if (equipment!=NULL){
-		vector<int> oldStat = equipment->getStat();
-        hpmax -= oldStat[0];
-        atk -= oldStat[1];
-        def -= oldStat[2];
-        magic -= oldStat[3];
-        speed -= oldStat[4];
-    }
-		equipment = a;
-		vector<int> stat = equipment->getStat();
-		hpmax += stat[0];
-		atk += stat[1];
-		def += stat[2];
-        magic += stat[3];
-        speed += stat[4];
-	
+
+
+	/*
+void Unit::equip(Equipment* a) {
+    if (equipmentList.size() >= 3) {
+        cout << "Cannot equip more than 3 items." << endl;
+        return;}
+
+
+	equipmentList.push_back(a);
+    vector<int> stat = a->getStat();
+    hpmax += stat[0];
+    atk += stat[1];
+    def += stat[2];
+    magic += stat[3];
+    speed += stat[4];
 
 	if (hp>hpmax)
 	{
 		hp=hpmax;
 	}
+
+
+	void Unit::unequip(int index) {
 	
-	}*/
+	vector<int> stat = equipmentList[index]->getStat();
+    hpmax -= stat[0];
+    atk -= stat[1];
+    def -= stat[2];
+    magic -= stat[3];
+    speed -= stat[4];
+
+    equipmentList.erase(equipmentList.begin() + index);
+
+	if (hp > hpmax) {
+        hp = hpmax;
+    }
+    }*/
