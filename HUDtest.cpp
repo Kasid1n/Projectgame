d5fcf474b420cea3a900a27618b197779b804235
kasidin <kasidinkhamfoo@gmail.com>
2025-02-22 23:09:34 +0700

    Updated

--- HUDtest.cpp
+++ HUDtest.cpp
@@ -11,6 +11,7 @@ void nHUD();
 void showmon(string); //entity show
 void doASCii(string); //ASCii Art
 void doSlow(string); //Text Delay
+void doSlowF(string);
 HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);//Text setting
 
 void doASCii(string x){
@@ -34,10 +35,32 @@ void doSlow(const string &text, int delay) {
     }
     cout<<endl;
 }
-    
+void doSlowF(const string &text, int delay){
+    string line="";
+    ifstream inFile;
+    inFile.open(text);
+    if(inFile.is_open())
+    {
+        while(getline(inFile,line)){
+            
+            Sleep(10);  // Reduce CPU usage  
+            for (char c : line) {
+                if (GetAsyncKeyState(VK_LCONTROL) & 0x8000) {  // Ctrl to Skip
+                    continue; 
+                }
+                cout << c << flush;
+                Sleep(delay);  // Delay in milliseconds
+            }
+            cout<<endl;
+        }
+    }
+    else
+    cout<<"File failed to load";
+    inFile.close();
+}
 
 void sHUD(){
-    SetConsoleTextAttribute(h,4);//Text Color
+    SetConsoleTextAttribute(h,4);//Text Color Red
     string filetxt="ascii_folder/text.txt";//Game Title
     doASCii(filetxt);
 }
@@ -48,10 +71,12 @@ void nHUD(){
     cout<<"Name HP : 1/1"<<setw(20)<<"Gold : 500\n";
     cout<<"=====================================================================================\n";
 }
+ 
 
 void showmon(string x){
+    SetConsoleTextAttribute(h,2);//Green
     cout<<"=====================================================================================\n";
-    
+    cout<<"                                    Mon                                              \n";
     cout<<"=====================================================================================\n";
     SetConsoleTextAttribute(h,4);//Red
     string filetxt=x;
@@ -69,7 +94,16 @@ int main(){
         Sleep(10);  // Reduce CPU usage
     }
     system("cls"); 
-    showmon("ascii_folder/Skele1.txt");
+    doSlowF("ascii_folder/Story1.txt",120);
+    cout<<"Press Spacebar to continue.";  
+    while (true) {
+        if (GetAsyncKeyState(VK_SPACE) & 0x8000) {  // Check if space bar is pressed
+            break;
+        }
+        Sleep(10);  // Reduce CPU usage
+    }  
+    system("cls");
+    showmon("ascii_folder/Skeleton.txt");
     nHUD();   
 } 
 
