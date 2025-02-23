937c5517600ee90801f749dafee5bf2dc287f3ff
Gtareiei <Natthapong0848@gmail.com>
2025-02-23 21:38:20 +0700

    level.cpp

--- level.cpp
+++ level.cpp
@@ -7,87 +7,79 @@
  -ดู class -
  ---randdom name ..
 */ 
-#include<iostream>
-#include<ctime>
-#include<vector>
-#include<cstdlib>
-#include<string>
-using namespace std ;
-class Player {
-private:
-    int level;
-    int xp;
-    int xptolevelup;
-    string name ;
-    int hp;
-    int hpmax;
-    int attack;
-    int defense; 
-    int magic;
-    int speed;
+#include "level.h"
+#include "Equipment.h"
 
+Player::Player(string Playername, int hpmax, int attack, int defense, int magic) {
+    name = Playername;
+    level = 1;
+    xp = 0;
+    xptolevelup = 100;
+    hp = hpmax;
+    hpmax = hpmax;  //chat บอกให้ใส่ this
+    this->attack = attack;
+    this->defense = defense;
+    this->magic = magic;
+    gold = 0;
+}
 
-public:
-    Player(string Playername) {
-        name = Playername ;
-        level = 1;
-        xp = 0;
-        xptolevelup = 100 ;
-
-        
+void Player::addxp(int xpgained) {
+    xp += xpgained;
+    while (xp >= xptolevelup && level < 100) {
+        levelup();
     }
-
-    void addxp(int xpgained) {
-        xp += xpgained;
-        while (xp >= xptolevelup && level < 100)  {   
-            levelup();
-        } if (level == 100)
-        {
-            xp = xptolevelup ;
-        }
+    if (level == 100) {
+        xp = xptolevelup;
     }
+}
 
-    
-    void showstatus() const {
-        cout << name<< " is level " << level << endl;
-        cout << name<<" xp is " << xp << endl;
-    }
+void Player::showstatus() const {
+    cout << name << " is level " << level << endl;
+    cout << name << " xp is " << xp << endl;
+    cout << "HP: " << hp << "/" << hpmax << endl;
+    cout << "Attack: " << attack << endl;
+    cout << "Defense: " << defense << endl;
+    cout << "Magic: " << magic << endl;
+    cout << "Gold: " << gold << endl;
+}
 
-private:
-    
-    void levelup() {
-        if(level < 100) {
+void Player::levelup() {
+    if (level < 100) {
         level++;
         xp -= xptolevelup;
-        xptolevelup += (50 * level)+(rand()%99+1) ;
-        cout <<name<<" reached level " << level << "!!!!" << endl;
-        showstatus(); }
-    }
-
-public:
-    
-    void dead() {
-        level = 1;
-        xp = 0;
-        xptolevelup = 100;
+        xptolevelup += (50 * level) + (rand() % 99 + 1);
+        cout << name << " reached level " << level << "!!!!" << endl;
         showstatus();
     }
-};
-
-/*
-int main() {
-    string name ;
-    cout<<"Please enter your name :" ;
-    getline(cin,name) ;
-    Player player(name) ;
-    player.addxp(9000000000);   
-   
-
-    player.dead();       
-
-    return 0;
 }
-*/
+
+void Player::dead() {
+    level = 1;
+    xp = 0;
+    xptolevelup = 100;
+    showstatus();
+}
+
+int Player::getLevel() const {
+    return level;
+}
+
+int Player::getGold() const {
+    return gold;
+}
+
+void Player::updateGold(int amount) {
+    gold += amount;
+}
+
+void Player::equipItem(const Equipment& item) {
+    vector<int> stats = item.getStat();
+    hpmax += stats[0];
+    attack += stats[1];
+    defense += stats[2];
+    magic += stats[3];
+}
+
 
 
 
