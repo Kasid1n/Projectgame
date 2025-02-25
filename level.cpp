b232b1aeafeaeb96b09b4c8fb24cd2d3aff5b759
Gtareiei <Natthapong0848@gmail.com>
2025-02-25 21:45:40 +0700

    level.cpp

--- level.cpp
+++ level.cpp
@@ -6,24 +6,31 @@
  ***dead*** re ?? 
  -ดู class -
  ---randdom name ..
+
+✅ เพิ่มระบบขายไอเท็มกลับร้านค้า
+✅ เพิ่มระบบ Inventory ให้ผู้เล่นเก็บไอเท็มก่อนสวมใส่
+✅ เพิ่มระบบโจมตีหรือใช้ไอเท็ม
+✅ เพิ่มระบบบันทึกและโหลดข้อมูลตัวละคร
 */ 
 #include "level.h"
 #include "Equipment.h"
+#include <ctime>
 
 Player::Player(string Playername, int hpmax, int attack, int defense, int magic) {
-    name = Playername;
-    level = 1;
-    xp = 0;
-    xptolevelup = 100;
-    hp = hpmax;
-    hpmax = hpmax;  //chat บอกให้ใส่ this
+    this->name = Playername;
+    this->hpmax = hpmax;
+    this->hp = hpmax;  // กำหนดค่าเริ่มต้นให้ hp = hpmax
     this->attack = attack;
     this->defense = defense;
     this->magic = magic;
-    gold = 0;
+    this->level = 1;
+    this->xp = 0;
+    this->xptolevelup = 100;
+    this->gold = 0;
 }
 
-void Player::addxp(int xpgained) {
+
+void Player::addxp(int xpgained) {      //เพิ่ม xp และให้เวลตัน 100
     xp += xpgained;
     while (xp >= xptolevelup && level < 100) {
         levelup();
@@ -33,7 +40,7 @@ void Player::addxp(int xpgained) {
     }
 }
 
-void Player::showstatus() const {
+void Player::showstatus() const {  //แสดงค่าสถานะ
     cout << name << " is level " << level << endl;
     cout << name << " xp is " << xp << endl;
     cout << "HP: " << hp << "/" << hpmax << endl;
@@ -43,24 +50,29 @@ void Player::showstatus() const {
     cout << "Gold: " << gold << endl;
 }
 
-void Player::levelup() {
+void Player::levelup() {  //เวลอัพ
     if (level < 100) {
         level++;
         xp -= xptolevelup;
-        xptolevelup += (50 * level) + (rand() % 99 + 1);
+        xptolevelup += (100 * level); //ใส่มั่ว
         cout << name << " reached level " << level << "!!!!" << endl;
         showstatus();
     }
 }
 
-void Player::dead() {
+void Player::dead() {  //ตาย re ค่า
     level = 1;
     xp = 0;
     xptolevelup = 100;
+    hp = hpmax = 100 ;
+    attack = 10 ;
+    defense = 5 :
+    magic = 5 ;
+    gold  =  0 ;
     showstatus();
 }
 
-int Player::getLevel() const {
+int Player::getLevel() const {  
     return level;
 }
 
@@ -68,11 +80,11 @@ int Player::getGold() const {
     return gold;
 }
 
-void Player::updateGold(int amount) {
+void Player::updateGold(int amount) {  
     gold += amount;
 }
 
-void Player::equipItem(const Equipment& item) {
+void Player::equipItem(const Equipment& item) {  // ใส่ของจากร้านค้า
     vector<int> stats = item.getStat();
     hpmax += stats[0];
     attack += stats[1];
