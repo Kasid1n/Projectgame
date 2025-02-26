6884ab1a44c9c898204b3f1e1fe0bad397dc18ac
Jon <ntjeddelog@gmail.com>
2025-02-26 08:52:05 +0700

    Update PVP.h

--- PVP.h
+++ PVP.h
@@ -17,7 +17,9 @@ public:
 Player player = {"Hero", 100, 100, 20, 10, 15};
 Monster enemy = {"Ajarn Daeng", 500, 500, 15, 8, 12};
 
-int AttackChoice() {
+/// จำลอง ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int AttackChoice() {/*อันนี้ช้อยของเพลเยอร์*/
     int achoice = 0;
     cout << "You're Attacker\n";
     while (achoice < 1 || achoice > 4) {
@@ -25,7 +27,7 @@ int AttackChoice() {
         cin >> achoice;
     }
     return achoice;}
-int DefenseChoice() {
+int DefenseChoice() {/*อันนี้ช้อยของเพลเยอร์*/
     int dchoice = 0;
     cout << "You're The Defender\n";
     while (dchoice < 1 || dchoice > 4) {
@@ -33,10 +35,78 @@ int DefenseChoice() {
         cin >> dchoice;
     }
     return dchoice;}
-/// จำลอง //////////////////////////////////////
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*เป็นระบบการต่อสู้ คำนวน*/
+void battlesys(Player &A, Monster &B, int attack, int defense) {/*เพราะมอนกับผู้เล่นมันคนละคลาสกัน เลยต้องทำ2อัน*/ /*อันนี้สำหรับคนตีมอนกัน*/
+    cout <<endl;
+    cout << A.name << " Hp: " << A.hp << endl;
+    cout << B.name << " Hp: " << B.hp << endl;
+    
+    float dmg = 0, def = B.defense, temp, rdmg, hp;
+    bool countered = false;
 
-void battlesys(Player &A, Monster &B, int attack, int defense) {
+    switch (attack) {/*ตี*/
+        case 1:
+            cout << A.name << " attacks!\n";
+            dmg = A.attack;
+            rdmg = rand() % 31 - 15;
+            dmg += dmg * (rdmg / 100);
+
+            if(defense==attack){/*กัน*/
+                temp = rand() % 30 + 1;
+                dmg -= dmg * ((50 + temp) / 100);
+            }
+            break;
+        case 2:
+            cout << A.name << " uses skill!\n";
+            dmg = A.magic;
+            def = 0;
+            temp = rand() % 3 + 1;
+            if (temp == 1) dmg = 0;
+            
+            if(defense==attack){/*กัน*/
+                temp = rand() % 4 + 1;
+                if (temp != 1) dmg = 0;
+            }
+            break;
+        case 3:
+            cout << A.name << " uses ultimate!\n";
+            dmg = (A.attack + A.magic) * 2;
+
+            if(defense==attack){/*กัน*/
+                countered = true;/*ถ้ากดกัน ตรงกับอันติจะรีเทินดาเมท*/
+            }
+            break;
+        case 4:
+            temp = rand() % 20 + 1;
+            hp = A.hpmax * ((20 + temp) / 100.0);
+            A.hp += hp;
+            if (A.hp > A.hpmax) A.hp = A.hpmax;
+            cout << A.name << " heals +" << hp << " Hp\n";
+            return;
+    }
+    switch (defense) {/*อันนี้ 1-3ทำให้มัน coutเฉยๆว่าเลือกอะไรไป*/
+        case 1:cout << B.name << " blocks!\n";break;
+        case 2:cout << B.name << " dodges!\n";break;
+        case 3:cout << B.name << " counters!\n";break;
+        case 4:
+            cout << B.name << " gave up!\n";
+            dmg = 9999999;
+            break;
+    }
+
+    if (countered==true) {/*รีเทินดาเมทของอันติ*/
+        A.hp -= dmg;
+    } else {/*โดนดาเมทธรรมดา*/
+        B.hp -= dmg;
+    }
+    cout << A.name << " did " << dmg << " damage\n";
+    cout << B.name << " lost " << dmg << " HP\n";
+    cout <<endl;
+}
+
+void battlesys(Monster &A, Player &B, int attack, int defense) {/*มอนกันคนตี*/ /*ด้านในเหมือนด้านบน แค่สลับinputเฉยๆ*/
     cout <<endl;
     cout << A.name << " Hp: " << A.hp << endl;
     cout << B.name << " Hp: " << B.hp << endl;
@@ -103,86 +173,16 @@ void battlesys(Player &A, Monster &B, int attack, int defense) {
     cout << B.name << " lost " << dmg << " HP\n";
     cout <<endl;
 }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-
-void battlesys(Monster &A, Player &B, int attack, int defense) {
-    cout <<endl;
-    cout << A.name << " Hp: " << A.hp << endl;
-    cout << B.name << " Hp: " << B.hp << endl;
-    
-    float dmg = 0, def = B.defense, temp, rdmg, hp;
-    bool countered = false;
-
-    switch (attack) {
-        case 1:
-            cout << A.name << " attacks!\n";
-            dmg = A.attack;
-            rdmg = rand() % 31 - 15;
-            dmg += dmg * (rdmg / 100);
-
-            if(defense==attack){
-                temp = rand() % 30 + 1;
-                dmg -= dmg * ((50 + temp) / 100);
-            }
-            break;
-        case 2:
-            cout << A.name << " uses skill!\n";
-            dmg = A.magic;
-            def = 0;
-            temp = rand() % 3 + 1;
-            if (temp == 1) dmg = 0;
-            
-            if(defense==attack){
-                temp = rand() % 4 + 1;
-                if (temp != 1) dmg = 0;
-            }
-            break;
-        case 3:
-            cout << A.name << " uses ultimate!\n";
-            dmg = (A.attack + A.magic) * 2;
-
-            if(defense==attack){
-                countered = true;
-            }
-            break;
-        case 4:
-            temp = rand() % 20 + 1;
-            hp = A.hpmax * ((20 + temp) / 100.0);
-            A.hp += hp;
-            if (A.hp > A.hpmax) A.hp = A.hpmax;
-            cout << A.name << " heals +" << hp << " Hp\n";
-            return;
-    }
-    switch (defense) {
-        case 1:cout << B.name << " blocks!\n";break;
-        case 2:cout << B.name << " dodges!\n";break;
-        case 3:cout << B.name << " counters!\n";break;
-        case 4:
-            cout << B.name << " gave up!\n";
-            dmg = 9999999;
-            break;
-    }
-
-    if (countered==true) {
-        A.hp -= dmg;
-    } else {
-        B.hp -= dmg;
-    }
-    cout << A.name << " did " << dmg << " damage\n";
-    cout << B.name << " lost " << dmg << " HP\n";
-    cout <<endl;
-}
-
-
-void battle(Player &player, Monster &enemy) {
+void battle(Player &player, Monster &enemy) {//ฟังก์ชั่นbattle ใช้วนจนhpฝ่ายนึงหมด
     int WR;
     cout << "Battle begins: " << player.name << " vs " << enemy.name << "!\n";
 
     int rcoin = rand() % 2, coin = 2;
     string scoin;
     
-    do {
+    do {/*สุ่มเหรียญ*/
         cout << "Pick head or tail: ";
         cin >> scoin;
         if (scoin == "head") coin = 0;
@@ -191,22 +191,22 @@ void battle(Player &player, Monster &enemy) {
     } while (coin == 2);
 
     int round = 1;
-    while (player.hp > 0 && enemy.hp > 0) {
+    while (player.hp > 0 && enemy.hp > 0) {/*ถ้าเลือดหมดจะจบ*/
         cout<<endl;
-        int aiattack = rand() % 4 + 1;
+        int aiattack = rand() % 4 + 1;/*มอนสุ่มช้อยตีกับกัน*/
         int aidefense = rand() % 3 + 1;
         int attack, defense;
 
         cout << "Round: " << round << endl;
 
-        if (coin == rcoin) {
+        if (coin == rcoin) {//สำหรับเหรีียญตรง จะได้เป็นattackerก่อน
             attack = AttackChoice();
-            battlesys(player, enemy, attack, aidefense);
+            battlesys(player/*คนตี*/, enemy/*คนกัน*/, attack/*ช้อยตีว่ากดตีแบบไหน*/, aidefense/*ช้อยกัน*/);
             if (enemy.hp <= 0 || player.hp <= 0) break;
             defense = DefenseChoice();
             battlesys(enemy, player, aiattack, defense);
             if (enemy.hp <= 0 || player.hp <= 0) break;
-        } else {
+        } else {//เหรียญไม่ตรง
             defense = DefenseChoice();
             battlesys(enemy, player, aiattack, defense);
             if (enemy.hp <= 0 || player.hp <= 0) break;
@@ -217,10 +217,10 @@ void battle(Player &player, Monster &enemy) {
         round++;
     }
 
-    if (player.hp > 0){WR=1; cout << "\n" << player.name << " wins!\n";}
+    if (player.hp > 0){WR=1; cout << "\n" << player.name << " wins!\n";}/*เช็คว่าใครชนะ*/
     else {WR=2; cout << "\n" << enemy.name << " wins!\n";}
     cout<<endl;}
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 int main() {
