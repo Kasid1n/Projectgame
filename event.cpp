d385d7ba17a4d826e8f37b64e2a25a31b108faab
kasidin <kasidinkhamfoo@gmail.com>
2025-02-27 19:27:03 +0700

    เหลือแค่นี้

--- event.cpp
+++ event.cpp
@@ -2,11 +2,11 @@
 #include <string>
 #include<ctime>
 using namespace std;
+#include"HUDtest.cpp"
 
 void mon();
 void shop();
 void box();
-void npc();
 void treasure();
 void boss();
 
@@ -14,33 +14,27 @@ void event(int &i){
    int r = rand()%100+1;
 
   if(i<10) {
-    if(r<=70){
+    if(r<=80){
       mon;
     }
-    else if(r>70 && r<=85){
+    else if(r>80 && r<=95){
       shop;
     }
-    else if(r>85 && r<=90){
+    else if(r>95 && r<=100){
       box;
-    }
-    else if(r>90 && r<=100){
-      npc;
     }
     i++;
   }
 
   else if(i>=10>30){
-        if(r<=65){
+        if(r<=75){
           mon;
         }
-        else if(r>65 && r<=75){
+        else if(r>75 && r<=85){
           shop;
         }
-        else if(r>75 && r<=80){
+        else if(r>85 && r<=90){
           box;
-        }
-        else if(r>80 && r<=90){
-          npc;
         }
         else if(r>90 && r<=95){
           treasure;
@@ -51,19 +45,16 @@ void event(int &i){
         i++;
   }
   else if(i>=30){
-    if(r<=45){
+    if(r<=55){
       mon;
     }
-    else if(r>45 && r<=55){
+    else if(r>55 && r<=65){
       shop;
     }
-    else if(r>55 && r<=60){
+    else if(r>65 && r<=70){
       box;
     }
-    else if(r>60 && r<=65){
-      npc;
-    }
-    else if(r>65 && r<=80){
+    else if(r>70 && r<=80){
       treasure;
     }
     else if(r>80 && r<=100){
@@ -74,43 +65,26 @@ void event(int &i){
 
 }
 
-
-
-void box(){
-  int r = rand()%4;
-  if (r<=2)
-  {
-    //get item
-    //get heal 1+rand()%10?    
-  }
-
-  if (r==4)
-  {
-    // -hp 1+rand()%10
-    // lose item
-  }
+void mon(){
+  
+  doSlow("You encounter a monster!",50);
 
 }
-/* npc -talk,got iteam-heal-gold ,lose iteam-hp minigame?
-*/
-// void npc() {
-//     int action = rand() % 4;
 
-//     if (action == 0) {
-//         // NPC talks
-//         cout << "NPC: \"Hello, traveler! XXXXX\"" << endl;
-//     }
-//     if (action == 1) {
-//          int heal = 1 + rand() % 10;
-//          cout << "NPC healing  " << heal << endl;
-//     }
-//    if (action == 2){
-//          int gold = 1 + rand() % 10;
-//          cout << "You got " << gold << " gold." << endl;
-//     }
-//     else {
-//          int gold = 1 + rand() % 10;
-//          cout << "You were robbed.\n You lose " << gold << " gold." << endl;
-//    }
-// }
+void shop(){
+
+}
+
+void treasure(){
+
+}
+
+void boss(){
+
+}
+
+void box(){
+  
+}
+
    
