4ae307f88bd58e103f0ec3131d78d69690805a53
kasidin <kasidinkhamfoo@gmail.com>
2025-02-27 18:47:23 +0700

    up

--- HUDtest.cpp
+++ HUDtest.cpp
@@ -5,26 +5,27 @@
 #include<iomanip>
 #include<ctime>
 using namespace std;
-#include"z.cpp"
 
 
+#include"Equipment.cpp"
 
 
 //Screen Output
 void sHUD();//Title
 void nHUD();//Choice Hud
 void blank();//blank screnn
-
+void bar();//cout bar
 void doASCii(string); //ASCii Art
 void doSlow(string); //Text Delay
 void doSlowF(string);
 void showeap();//gear show
+void spaceb();//press space bar
 HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);//Text setting
 
 string hero;//player name
 int startweap;//select start weapon
 vector<int> gear(3);
-vector<Equipment> inven(3);
+//vector<Equipment> inven(3);
 void doASCii(string x){
     string line="";
     ifstream inFile;
@@ -71,29 +72,145 @@ void doSlowF(const string &text, int delay){
     inFile.close();
 }
 
+
+
 void showeap(){
     SetConsoleTextAttribute(h,7);
     char choice;
     blank();
-    cout<<"=====================================================================================\n";
+    bar();
     cout<<"Which one...\n";
-    cout<<"[1]  [2]  [3]  [E] Quit. \n";
+    cout<<"[1]  [2]  [3]  [E] Quit. \n>";
     while (true){
         cin>> choice;
     switch(choice){
         case '1':
         switch(gear.at(0)){
             case 1:
-            doASCii("ascii_folder/Axe.txt");
-            break;
+                system("cls");
+                SetConsoleTextAttribute(h,2);
+                doASCii("ascii_folder/Axe.txt");
+                SetConsoleTextAttribute(h,7);
+                bar();
+                cout<<"Axe | Stat";
+                cout<<"\nPress Spacebar to go back.";
+                spaceb();
+                system("cls");
+                blank();
+                nHUD();
+                break;
             case 2:
-            doASCii("ascii_folder/Bow.txt");
-            break;
+                system("cls");
+                SetConsoleTextAttribute(h,2);
+                doASCii("ascii_folder/Bow.txt");
+                SetConsoleTextAttribute(h,7);
+                bar();
+                cout<<"Bow | Stat";
+                cout<<"\nPress Spacebar to go back.";
+                spaceb();
+                system("cls");
+                blank();
+                nHUD();
+                break;
             case 3:
-            doASCii("ascii_folder/Sword.txt");
-            break;
+                system("cls");
+                SetConsoleTextAttribute(h,2);
+                doASCii("ascii_folder/Sword.txt");
+                SetConsoleTextAttribute(h,7);
+                bar();
+                cout<<"Sword | Stat";
+                cout<<"\nPress Spacebar to go back.";
+                spaceb();
+                system("cls");
+                blank();
+                nHUD();
+                break;
+            case 4:
+                system("cls");
+                SetConsoleTextAttribute(h,2);
+                doASCii("ascii_folder/Shield.txt");
+                SetConsoleTextAttribute(h,7);
+                bar();
+                cout<<"Shield | Stat";
+                cout<<"\nPress Spacebar to go back.";
+                spaceb();
+                system("cls");
+                blank();
+                nHUD();
+                break;
+            case 5:
+                system("cls");
+                SetConsoleTextAttribute(h,3);
+                doASCii("ascii_folder/BetterAxe.txt");
+                SetConsoleTextAttribute(h,7);
+                bar();
+                cout<<"Waraxe | Stat";
+                cout<<"\nPress Spacebar to go back.";
+                spaceb();
+                system("cls");
+                blank();
+                nHUD();
+                break;
+
+            case 6:
+                system("cls");
+                SetConsoleTextAttribute(h,3);
+                doASCii("ascii_folder/BetterSword.txt");
+                SetConsoleTextAttribute(h,7);
+                bar();
+                cout<<"Da Sword | Stat";
+                cout<<"\nPress Spacebar to go back.";
+                spaceb();
+                system("cls");
+                blank();
+                nHUD();
+                break;
+
+            case 7:
+                system("cls");
+                SetConsoleTextAttribute(h,3);
+                doASCii("ascii_folder/BetterShield.txt");
+                SetConsoleTextAttribute(h,7);
+                bar();
+                cout<<"Saint'Wall | Stat";
+                cout<<"\nPress Spacebar to go back.";
+                spaceb();
+                system("cls");
+                blank();
+                nHUD();
+                break;
+
+            case 8:
+                system("cls");
+                SetConsoleTextAttribute(h,2);
+                doASCii("ascii_folder/Armor.txt");
+                SetConsoleTextAttribute(h,7);
+                bar();
+                cout<<"Armor | Stat";
+                cout<<"\nPress Spacebar to go back.";
+                spaceb();
+                system("cls");
+                blank();
+                nHUD();
+                break;
+
+            case 9:
+                system("cls");
+                SetConsoleTextAttribute(h,2);
+                doASCii("ascii_folder/Necklace.txt");
+                SetConsoleTextAttribute(h,7); 
+                bar();
+                cout<<"Necklace | Stat";
+                cout<<"\nPress Spacebar to go back.";
+                spaceb();
+                system("cls");
+                blank();
+                nHUD();
+                break;
+
+            
         }
-        break;
+            break;
         case '2':
         switch(gear.at(1)){
             case 1:
@@ -105,7 +222,7 @@ void showeap(){
             case 3:
             doASCii("ascii_folder/Sword.txt");
             break;
-        }
+        }  
 
         doASCii("ascii_folder/Bow.txt");
         break;
@@ -128,12 +245,18 @@ void showeap(){
         }
         
         break;
-        case 'e'||'E':
+        case 'e':
         system("cls");
         blank();
         nHUD();
         break;
 
+        case 'E':
+        system("cls");
+        blank();
+        nHUD();
+        break;
+ 
         default:
         cout<<"You only have 3 slot of Equipment.\n";
     }
@@ -142,7 +265,14 @@ void showeap(){
     
 }
 
-
+void spaceb(){
+    while (true) {
+            if (GetAsyncKeyState(VK_SPACE) & 0x8000) {  // Check if space bar is pressed
+                break;
+            }
+            Sleep(10);  // Reduce CPU usage
+    }
+}
 
 void sHUD(){
     SetConsoleTextAttribute(h,15);//Text Color Red
@@ -158,8 +288,8 @@ void nHUD(){
     cout<<"=====================================================================================\n";
     cout<<hero<<setw(15)<<"| HP : 1/1"<<setw(15)<<"Level : 1 "<<setw(15)<<"Gold : 500\n";  
     cout<<"=====================================================================================\n";
-    doSlow("You decide to :\n",100);
-    cout<<"[1] Moving Forward"<<setw(15)<<"[2] Rest"<<setw(30)<<"[3] Check your equipment";
+    doSlow("You decide to :\n",50);
+    cout<<"[1] Moving Forward"<<setw(15)<<"[2] Rest"<<setw(30)<<"[3] Check your equipment\n>";
     int choice;
     
     int sc = rand()%50;//Different Scenery
@@ -172,7 +302,7 @@ void nHUD(){
         if(sc>=10){
         doASCii("ascii_folder/walls2.txt");
         cout<<"=====================================================================================\n";
-        doSlow("You walk past the dark hallway...",100);
+        doSlow("You walk past the dark hallway...",50);
         cout<<"\nPress Spacebar to continue.";
         while (true) {
             if (GetAsyncKeyState(VK_SPACE) & 0x8000) {  // Check if space bar is pressed
@@ -189,7 +319,7 @@ void nHUD(){
         }
         else {doASCii("ascii_folder/walls1.txt");
         cout<<"=====================================================================================\n";
-        doSlow("You found a gate and open it...",100);
+        doSlow("You found a gate and open it...",50);
         cout<<"\nPress Spacebar to continue.";
         while (true) {
             if (GetAsyncKeyState(VK_SPACE) & 0x8000) {  // Check if space bar is pressed
@@ -229,70 +359,79 @@ void nHUD(){
     }  
 }
  
+void bar(){
+    cout<<"=====================================================================================\n";
+}
 
 void blank(){
     for(int i =0;i<24;i++){
         cout<<endl;
     }
-}  
-// int main(){
-//     Equipment sword(0, 10, 0, 5);
-//     Equipment shield(0, 0, 10, 0);
-//     Equipment axe(0, 15, 0, 0);
-//     Equipment bow(0, 10, 0, 5);
+} 
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////
+int main(){
+    Equipment sword(0, 10, 0, 5);
+    Equipment shield(0, 0, 10, 0);
+    Equipment axe(0, 15, 0, 0);
+    Equipment bow(0, 10, 0, 5);
     
 
-//     SetConsoleOutputCP(65001);// Set CMD to UTF-8
-//     system("cls");//Clear Screen
-//     sHUD();
-//     while (true) {
-//         if (GetAsyncKeyState(VK_SPACE) & 0x8000) {  // Check if space bar is pressed
-//             break;
-//         }
-//         Sleep(10);  // Reduce CPU usage
-//     }
-//     system("cls");
-//     doSlow("Your name is...",100);
-//     cin>> hero;
-//     Stats hero(100, 20, 10, 5);
-//     doSlow("There's a weapon beside you... It is...",100);
-    
-//     cout<<"\n[1] Axe"<<setw(10)<<"[2] Bow"<<setw(10)<<"[3] Sword";
-//     while(true){
-//     cin>>startweap;
-//     switch(startweap){
-//         case 1:
-//         hero.equip(&axe);
-//         gear.at(0)=1;//gearid of axe
-//         break;
-//         case 2:
-//         hero.equip(&bow);
-//         gear.at(0)=2;//id bow
-//         break;
-//         case 3:
-//         hero.equip(&sword);
-//         gear.at(0)=3;//id sword
-//         break;
-//         default:
-//         cout<<"There's nothing else...";
-//     }
-//     break;
-//     } 
+    SetConsoleOutputCP(65001);// Set CMD to UTF-8
+    system("cls");//Clear Screen
+    sHUD();
+    while (true) {
+        if (GetAsyncKeyState(VK_SPACE) & 0x8000) {  // Check if space bar is pressed
+            break;
+        }
+        Sleep(10);  // Reduce CPU usage
+    }
+    system("cls");
+    doSlow("Your name is...",50);
+    cin>> hero;
+    Stats hero(100, 20, 10, 5);
+    doSlow("There's a weapon beside you... It is...",50);
+    cout<<"\n[1] Axe"<<setw(10)<<"[2] Bow"<<setw(10)<<"[3] Sword\n>";
+    while(true){
+    cin>>startweap;
+    switch(startweap){
+        case 1:
+        hero.equip(&axe);
+        gear.at(0)=1;//gearid of axe
+        
+        break;
+        case 2:
+        hero.equip(&bow);
+        gear.at(0)=2;//id bow
+        
+        break;
+        case 3:
+        hero.equip(&sword);
+        gear.at(0)=3;//id sword
+        
+        break;
+        default:
+        cout<<"There's nothing else...";
+    }
+    break;
+    } 
 
-//     system("cls");
-//     doSlowF("ascii_folder/Story1.txt",100);//Intro
-//     cout<<"\nPress Spacebar to continue.";  
-//     while (true) {
-//         if (GetAsyncKeyState(VK_SPACE) & 0x8000) {  // Check if space bar is pressed
-//             break;
-//         }
-//         Sleep(10);  // Reduce CPU usage
-//     }  
-//     system("cls");
-//     blank();
-//     nHUD();
+    system("cls");
+    doSlowF("ascii_folder/Story1.txt",50);//Intro
+    cout<<"\nPress Spacebar to continue.";  
+    while (true) {
+        if (GetAsyncKeyState(VK_SPACE) & 0x8000) {  // Check if space bar is pressed
+            break;
+        }
+        Sleep(10);  // Reduce CPU usage
+    }  
+    system("cls");
+    blank();
+    nHUD();
     
-// } 
+} 
 
 
 
