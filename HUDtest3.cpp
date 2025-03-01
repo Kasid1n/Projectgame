eea4e7654b8d24820595a663eb97a922a9ea65fc
Chiraphat-t <chiraphat.ttt@gmail.com>
2025-03-01 20:59:02 +0700

    0

--- HUDtest3.cpp
+++ HUDtest3.cpp
@@ -3,24 +3,11 @@
 #include<windows.h>
 #include <cstdlib>
 #include<iomanip>
-#include"test_4person.cpp"
+#include"test_4person.h"
+#include"Qol.h"
 using namespace std;
 
 
-void doASCii(string x){
-    string line="";
-    ifstream inFile;
-    inFile.open(x);
-    if(inFile.is_open())
-    {
-        while(getline(inFile,line)){
-            cout<<line<<endl;
-        }
-    }
-    else
-    cout<<"File failed to load";
-    inFile.close();
-}
 
 void showNPC();
 
@@ -29,19 +16,25 @@ void showNPC(Player A){
     cout << "║                                   SHOP                                     ║\n";
     cout << "╚════════════════════════════════════════════════════════════════════════════╝\n";
     NPC shopNPC = getRandomNPC();
-
+    
+    //npcname pb
     if(shopNPC.npcname=="A"){doASCii("ascii_folder/npc1.txt");}
     if(shopNPC.npcname=="B"){doASCii("ascii_folder/npc2.txt");}
     if(shopNPC.npcname=="Secretmaster"){doASCii("ascii_folder/npc3.txt");}  
     cout << "════════════════════════════════════════════════════════════════════════════\n";
-    shopNPC.sellItemToPlayer(A);  
-    
+    //shopNPC.sellItemToPlayer(A);  
+    shopNPC.shop();//doloop?
+
+    //system("cls");
 
 }
 
 int main(){
     SetConsoleOutputCP(65001);
     Player player("s", 100, 20, 10, 5);
+    player.addGold(100);
+    showNPC(player);
+    showNPC(player);
     showNPC(player);
     
 }
